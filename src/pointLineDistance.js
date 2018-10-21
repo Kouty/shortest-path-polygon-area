0bdf98cebc5156f18a5a33b68aff649ee2a8fcdd
Andrea Koutifaris <kouty79@gmail.com>
2018-10-21 17:23:08 +0200

    Point-line distance

---
+++ src/pointLineDistance.js
@@ -0,0 +1,5 @@
+export function pointLineDistance([x, y], [[x1, y1], [x2, y2]]) {
+  const numerator = Math.abs((y2 - y1) * x - (x2 - x1) * y + x2 * y1 - y2 * x1);
+  const denominator = Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
+  return numerator / denominator;
+}
