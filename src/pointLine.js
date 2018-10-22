05f2c37cb60224e3801ae271372ae1a019ac175e
Kouty <kouty79@gmail.com>
2018-10-22 09:16:47 +0200

    Moves functions into pointLine file

--- src/pointLine.js
+++ src/pointLine.js
@@ -3,3 +3,11 @@ export function pointLineDistance([x, y], [[x1, y1], [x2, y2]]) {
   const denominator = Math.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2);
   return numerator / denominator;
 }
+
+export function pointSide({ x, y }, [{ x: x1, y: y1 }, { x: x2, y: y2 }]) {
+  const d = (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1);
+  return d < 0 ? pointSide.LEFT : pointSide.RIGHT;
+}
+
+pointSide.LEFT = Symbol('pointSide.LEFT');
+pointSide.RIGHT = Symbol('pointSide.RIGHT');
--- test/pointLine.spec.js
+++ test/pointLine.spec.js
@@ -1,7 +1,28 @@
-import { pointLineDistance } from '../src/pointLine';
+import { pointLineDistance, pointSide } from '../src/pointLine';
 
 describe('Point-line math', () => {
-  it('(1,1) should be distance 1 from x axis', () => {
-    expect(pointLineDistance([1, 1], [[0, 0], [10, 0]])).toBeCloseTo(1);
+  describe('pointLineDistance', () => {
+    it('(1,1) should be distance 1 from x axis', () => {
+      expect(pointLineDistance([1, 1], [[0, 0], [10, 0]])).toBeCloseTo(1);
+    });
   });
+
+  describe('pointSide', () => {
+    it('should define a LEFT side', () => {
+      expect(pointSide.LEFT).toBeDefined();
+    });
+
+    it('should return LEFT for the point (2,3) and the line [(0,0), (3,3)]', () => {
+      expect(pointSide({ x: 2, y: 3 }, [{ x: 0, y: 0 }, { x: 3, y: 3 }])).toBe(pointSide.LEFT);
+    });
+
+    it('should define a RIGHT side', () => {
+      expect(pointSide.RIGHT).toBeDefined();
+    });
+
+    it('should return RIGHT for the point (2,1) and the line [(0,0), (3,3)]', () => {
+      expect(pointSide({ x: 2, y: 1 }, [{ x: 0, y: 0 }, { x: 3, y: 3 }])).toBe(pointSide.RIGHT);
+    });
+  });
+
 });
