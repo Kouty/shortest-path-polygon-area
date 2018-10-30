101520ad154f72a58769354ea2cb2708915d8a0f
Kouty <kouty79@gmail.com>
2018-10-30 18:28:48 +0100

    Rearranges segment-segmet tests

--- test/segmentSegment.spec.js
+++ test/segmentSegment.spec.js
@@ -1,47 +1,50 @@
 import { segSegIntersect } from '../src/segmentSegment';
 
 describe('Segment-segment', () => {
-  it('should define segSegIntersect.INTERSECTION', () => {
-    expect(segSegIntersect.INTERSECTION).not.toBeUndefined();
+
+  describe('results', () => {
+    it('should define segSegIntersect.INTERSECTION', () => {
+      expect(segSegIntersect.INTERSECTION).not.toBeUndefined();
+    });
+
+    it('should define segSegIntersect.NO_INTERSECTION', () => {
+      expect(segSegIntersect.NO_INTERSECTION).not.toBeUndefined();
+    });
   });
 
-  it('should considers segments [[0,0],[10,10]], [[0,10],[10,0]] as intersecting', () => {
-    expect(segSegIntersect([[0, 0], [10, 10]], [[0, 10], [10, 0]])).toBe(
-      segSegIntersect.INTERSECTION
-    );
+  describe('not inline segments', () => {
+    it('should considers segments [[0,0],[10,10]], [[0,10],[10,0]] as intersecting', () => {
+      expect(segSegIntersect([[0, 0], [10, 10]], [[0, 10], [10, 0]])).toBe(
+        segSegIntersect.INTERSECTION
+      );
+    });
+
+    it('should considers segments [[0,5],[5,5]], [[2,2],[7,2]] as NOT intersecting', () => {
+      expect(segSegIntersect([[0, 5], [5, 5]], [[2, 2], [7, 2]])).toBe(
+        segSegIntersect.NO_INTERSECTION
+      );
+    });
   });
 
-  it('should define segSegIntersect.INTERSECTION', () => {
-    expect(segSegIntersect.NO_INTERSECTION).not.toBeUndefined();
+  describe('inline segments', () => {
+    it('should considers segments [[0,5],[5,5]], [[4,5],[7,5]] as intersecting', () => {
+      expect(segSegIntersect([[0, 5], [5, 5]], [[4, 5], [7, 5]])).toBe(
+        segSegIntersect.INTERSECTION
+      );
+    });
+
+    it('should considers segments [[0,5],[5,5]], [[6,5],[7,5]] as NOT intersecting', () => {
+      expect(segSegIntersect([[0, 5], [5, 5]], [[6, 5], [7, 5]])).toBe(
+        segSegIntersect.NO_INTERSECTION
+      );
+    });
   });
 
-  it('should considers segments [[0,0],[5,5]], [[6,10],[7,0]] as NOT intersecting', () => {
-    expect(segSegIntersect([[0, 0], [5, 5]], [[6, 10], [7, 0]])).toBe(
-      segSegIntersect.NO_INTERSECTION
-    );
-  });
-
-  it('should considers segments [[0,5],[5,5]], [[2,2],[7,2]] as NOT intersecting', () => {
-    expect(segSegIntersect([[0, 5], [5, 5]], [[2, 2], [7, 2]])).toBe(
-      segSegIntersect.NO_INTERSECTION
-    );
-  });
-
-  it('should considers segments [[0,5],[5,5]], [[4,5],[7,5]] as intersecting', () => {
-    expect(segSegIntersect([[0, 5], [5, 5]], [[4, 5], [7, 5]])).toBe(
-      segSegIntersect.INTERSECTION
-    );
-  });
-
-  it('should considers segments [[0,5],[5,5]], [[6,5],[7,5]] as NOT intersecting', () => {
-    expect(segSegIntersect([[0, 5], [5, 5]], [[6, 5], [7, 5]])).toBe(
-      segSegIntersect.NO_INTERSECTION
-    );
-  });
-
-  it('should considers segments [[0,0],[5,5]], [[4.95, 4.95],[7,7]] as NOT intersecting with precision 0.1', () => {
-    expect(segSegIntersect([[0, 0], [5, 5]], [[4.95, 4.95], [7, 7]], 0.1)).toBe(
-      segSegIntersect.NO_INTERSECTION
-    );
+  describe('precision 0.1', () => {
+    it('should considers segments [[0,0],[5,5]], [[4.95, 4.95],[7,7]] as NOT intersecting with precision 0.1', () => {
+      expect(segSegIntersect([[0, 0], [5, 5]], [[4.95, 4.95], [7, 7]], 0.1)).toBe(
+        segSegIntersect.NO_INTERSECTION
+      );
+    });
   });
 });
