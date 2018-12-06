fb23941972d1f8cddfd7085c84f06dd44107448c
Kouty <kouty79@gmail.com>
2018-12-06 12:23:41 +0100

    Renamed .spec into .test

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.spec.js'
+      'test/**/*.test.js'
     ],
 
 
@@ -27,7 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/*.spec.js': ['webpack']
+      'test/*.test.js': ['webpack']
     },
 
     webpack: webpackConf,
