b642e146c776e4d7bc0b40aa7f9108dd60dba12f
Kouty <kouty79@gmail.com>
2018-12-06 16:24:30 +0100

    Impelemts Tree and bfIterator

---
+++ src/ShortestPath.js
@@ -0,0 +1,11 @@
+export class ShortestPath {
+  constructor(area) {
+    this._area = area;
+  }
+
+  calculate({ from, to }) {
+
+
+    return [from, to];
+  }
+}
---
+++ test/ShortestPath.test.js
@@ -0,0 +1,27 @@
+import { ShortestPath } from '../src/ShortestPath';
+
+describe('ShortestPath', () => {
+  let bounds;
+
+  beforeEach(() => {
+    bounds = [[0, 0], [10, 0], [10, 10], [0, 10]];
+  });
+
+  it('direct path is the shortest path with no holes', () => {
+    const holes = [];
+    const from = [1, 1];
+    const to = [9, 9];
+
+    const path = new ShortestPath({ bounds, holes }).calculate({ from, to });
+    expect(path).toEqual([from, to]);
+  });
+
+  xit('one hole', () => {
+    const holes = [[4, 4], [4, 6], [5, 5], [5, 4]];
+    const from = [1, 1];
+    const to = [9, 9];
+
+    const path = new ShortestPath({ bounds, holes }).calculate({ from, to });
+    expect(path).toEqual([from, [5, 4], to]);
+  });
+});
