ae76dd3f1dcbb3a43637afff78615d6f4f64776f
Kouty <kouty79@gmail.com>
2018-12-13 14:53:09 +0100

    Implements point in polygon inside

--- src/polygon.js
+++ src/polygon.js
@@ -6,14 +6,23 @@ export function polyPointInside(poly, point) {
   for (let i = 1; i <= poly.length + 1; i++) {
     const segment = [prevPoint, poly[i >= poly.length ? 0 : i]];
     const sideA = segmentPointSide(point, segment);
-    // const sideB = segmentPointSide([point[0], Number.POSITIVE_INFINITY], segment);
+    const sideB = segmentPointSide([point[0], Number.POSITIVE_INFINITY], segment);
 
     if (sideA === segmentPointSide.ABOVE) {
       return polyPointInside.ABOVE;
     }
 
+    if (
+      sideA === segmentPointSide.RIGHT && sideB === segmentPointSide.LEFT ||
+      sideA === segmentPointSide.LEFT && sideB === segmentPointSide.RIGHT
+    ) {
+      intersectionCounter++;
+    }
 
+    prevPoint = segment[1];
   }
+
+  return intersectionCounter % 2 === 0 ? null : polyPointInside.INSIDE;
 }
 
 // segmentPointSide.LEFT = Symbol('segmentPointSide.LEFT');
--- test/polygon.test.js
+++ test/polygon.test.js
@@ -3,7 +3,7 @@ import { polyPointInside } from '../src/polygon';
 describe('polyPointInside', () => {
   let square;
   const INSIDE = polyPointInside.INSIDE;
-  const OUTSIDE = polyPointInside.INSIDE;
+  const OUTSIDE = polyPointInside.OUTSIDE;
   const ABOVE = polyPointInside.ABOVE;
 
   beforeEach(() => {
@@ -22,11 +22,16 @@ describe('polyPointInside', () => {
     expect(polyPointInside.OUTSIDE).toBeDefined();
   });
 
-  // it('should return INSIDE for point [5,5]', () => {
-  //   expect(polyPointInside([5, 5], square)).toBe(INSIDE);
-  // });
-
   it('should return ABOVE for point [0,5]', () => {
     expect(polyPointInside(square, [0, 5])).toBe(ABOVE);
+  });
+
+  it('should return INSIDE for point [5,5]', () => {
+    expect(polyPointInside(square, [5, 5])).toBe(INSIDE);
+  });
+
+  it('should return INSIDE for point [5,5]', () => {
+    const counterClockSquare = square.reverse();
+    expect(polyPointInside(counterClockSquare, [5, 5])).toBe(INSIDE);
   });
 });
