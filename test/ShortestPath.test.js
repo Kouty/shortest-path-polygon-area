f40739eae00f6201acb39fdcd7430a3fc4d18d64
Kouty <kouty79@gmail.com>
2018-12-14 15:36:08 +0100

    Adds shortest-path over hole side test

--- test/ShortestPath.test.js
+++ test/ShortestPath.test.js
@@ -16,12 +16,21 @@ describe('ShortestPath', () => {
     expect(path).toEqual([from, to]);
   });
 
-  it('one hole', () => {
+  it('one hole, path on vertices', () => {
     const holes = [[[4, 4], [4, 6], [5, 5], [5, 4]]];
     const from = [1, 1];
     const to = [9, 9];
 
     const path = new ShortestPath({ bounds, holes }).calculate({ from, to });
     expect(path).toEqual([from, [5, 4], to]);
+  });
+
+  it('one hole, path on side of hole', () => {
+    const holes = [[[4, 4], [4, 6], [6, 5], [6, 4]]];
+    const from = [5, 3];
+    const to = [5, 7];
+
+    const path = new ShortestPath({ bounds, holes }).calculate({ from, to });
+    expect(path).toEqual([from, [6, 4], [6, 5], to]);
   });
 });
